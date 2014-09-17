3603258f28269a1ab9aa409bdc6bf547c9a612a4
徳重 卓朗 <tokushige@MBP-Retina.local>
2014-09-17 19:56:38 +0900

    dotfilelink.sh

---
+++ dotfilelink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d "~/.dotfile" ];then
+    mkdir ~/.dotfile
+fi
+
+cp -pr . ~/.dotfile/
+ln -sf ~/.dotfile/.vimrc ~/.vimrc
+ln -sf ~/.dotfile/.gvimrc ~/.gvimrc
+ln -sf ~/.dotfile/.bashrc ~/.bashrc
+ln -sf ~/.dotfile/.bash_profile ~/.bash_profile
+ln -sf ~/.dotfile/snippets ~/.vim/
+
